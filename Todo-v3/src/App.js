2f010c0f575b4f3fd92b77350fcd48b311f14291
Rishi Ranjan <97913174+RishiRanjan-01@users.noreply.github.com>
2022-05-27 00:04:09 +0530

    Add files via upload

---
+++ Todo-v3/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div className='maindiv'>
+      <h1 className='header'>TODO TASK</h1>
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
